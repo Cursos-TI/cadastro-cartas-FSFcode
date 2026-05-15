dfade7cf3f949c8c38a35464acbf6dd4bf59e007
FSFcode <202602088908@alunos.estacio.br>
2026-05-15 03:01:20 +0000

    Realizei o nível aventureiro do desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,7 +13,9 @@ int main()
   int populacao[2];
   int pontos[2];
   float pib[2];
-  float area[2];
+  float area[2]; 
+  float pibCapita[2];
+  float densidadePop[2];
 
   // Área para entrada de dados
   // Cadasrtro carta 1
@@ -27,7 +29,7 @@ int main()
   scanf(" %s", codigo[0]);
 
   printf("Digite o nome da cidade: ");
-  scanf(" %s", cidade[0]);
+  scanf(" %[^\n]", cidade[0]);
 
   printf("Digite o número de habitantes da sua cidade: ");
   scanf(" %d", &populacao[0]);
@@ -38,13 +40,13 @@ int main()
   printf("Digite a área (km²) da cidade: ");
   scanf(" %f", &area[0]);
 
-  printf("Digite o PIB sa cidade: R$");
+  printf("Digite o PIB da cidade: R$");
   scanf(" %f", &pib[0]);
 
   printf("A primeira carta foi cadastrada com sucesso!!!");
 
   // Cadastro carta 2
-  printf("\n\n---------\nCARTA DOIS\n---------\nVamos cadastrar a segunda carta do jogo!");
+  printf("\n\n---------\nCARTA DOIS\n---------\nVamos cadastrar a segunda carta do jogo!\n");
 
   printf("Defina uma letra de 'A' a 'H' (representando um dos oito estados): ");
   scanf(" %s", estado[1]);
@@ -53,7 +55,7 @@ int main()
   scanf(" %s", codigo[1]);
 
   printf("Digite o nome da cidade: ");
-  scanf(" %s", cidade[1]);
+  scanf(" %[^\n]", cidade[1]);
 
   printf("Digite o número de habitantes da sua cidade: ");
   scanf(" %d", &populacao[1]);
@@ -64,18 +66,27 @@ int main()
   printf("Digite a área (km²) da cidade: ");
   scanf(" %f", &area[1]);
 
-  printf("Digite o PIB ca cidade: R$");
+  printf("Digite o PIB da cidade: R$");
   scanf(" %f", &pib[1]);
 
   printf("A segunda carta foi cadastrada com sucesso!!!");
 
 
+  //Cálculos dos dados das cartas
+  // Carta 1
+  densidadePop[0] = populacao[0]/area[0];
+  pibCapita[0] = pib[0]/populacao[0];
+
+  //Carta 2
+  densidadePop[1] = populacao[1]/area[1];
+  pibCapita[1] = pib[1]/populacao[1];
+
   //Exibição dos dados das cidades
   printf("\n\n---------\nCARTA UM\n---------\n");
-  printf("Estado: %s\nCodigo: %s\nCidade: %s\nPopulação: %d\nPontos turísticos: %i\nÁrea: %.2fKm²\nPIB: R$%.2f",estado[0], codigo[0], cidade[0], populacao[0], pontos[0], area[0], pib[0]);
+  printf("Estado: %s\nCodigo: %s\nCidade: %s\nPopulação: %d\nPontos turísticos: %i\nÁrea: %.2fKm²\nPIB: R$%.2f\nDensidade Populacional: %.5f habitantes por Km²\nPib per Capita: R$%.2f",estado[0], codigo[0], cidade[0], populacao[0], pontos[0], area[0], pib[0], densidadePop[0], pibCapita[0]);
 
   printf("\n\n---------\nCARTA DOIS\n---------\n");
-  printf("Estado: %s\nCodigo: %s\nCidade: %s\nPopulação: %d\nPontos turísticos: %i\nÁrea: %.2fKm²\nPIB: R$%.2f",estado[1], codigo[1], cidade[1], populacao[1], pontos[1], area[1], pib[1]);
+  printf("Estado: %s\nCodigo: %s\nCidade: %s\nPopulação: %d\nPontos turísticos: %i\nÁrea: %.2fKm²\nPIB: R$%.2f\nDensidade Populacional: %.5f habitantes por Km²\nPib per Capita: R$%.2f",estado[1], codigo[1], cidade[1], populacao[1], pontos[1], area[1], pib[1], densidadePop[1], pibCapita[1]);
 
 
 
